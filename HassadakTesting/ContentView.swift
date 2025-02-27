dfd13d427eee9e7b2e7a9e3f02fb1402a870dce8
itsBashayer <alfuhaidbashayer@gmail.com>
2025-02-27 11:00:30 +0300

    Initial Commit

---
+++ HassadakTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HassadakTesting
+//
+//  Created by BASHAER AZIZ on 28/08/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HassadakTesting/HassadakTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HassadakTestingApp.swift
+//  HassadakTesting
+//
+//  Created by BASHAER AZIZ on 28/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HassadakTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
